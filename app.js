996d8b6bd9fe99dd41f3f5fcd9441538d3cd72df
Graham Ballantyne <grahamb@sfu.ca>
2013-06-04 15:26:33 -0700

    change get to post

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ app.configure('development', function() {
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
