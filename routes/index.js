252dfad9ccb11b00bb363254230a478b8d834346
Graham Ballantyne <grahamb@sfu.ca>
2013-06-04 15:26:49 -0700

    fetch data from library api and render view

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,22 @@
-  res.render('index', { title: 'Express' });
 exports.index = function(req, res) {
+
+    var request = require('request');
+
+    // get the course id
+    var courseSisId = req.body.lis_course_offering_sourcedid.split('-')
+      , qs = {
+            semester: courseSisId[0],
+            department: courseSisId[1],
+            number: courseSisId[2],
+            section: courseSisId[3]
+    };
+
+    request({
+        url: 'http://api.lib.sfu.ca/reserves/search',
+        qs: qs
+    }, function(err, resp, body) {
+        console.log(resp);
+        res.render('index', { data: JSON.parse(body), course: req.body, title: 'Library Reserves' });
+    });
+
 };
