0598c55d255953d4d2ca6268cdb528bd46da3402
Graham Ballantyne <grahamb@sfu.ca>
2013-08-12 14:46:45 -0700

    add type param to endpoint url

--- components/courseDescription/public/javascripts/app.js
+++ components/courseDescription/public/javascripts/app.js
@@ -4,7 +4,7 @@ var redirectToCanvas = function() {
         lti.description,
         ' &ndash; <a href="' + lti.courseUrl + '" target=_blank><em>Source: SFU Academic Calendar</em></a></p>'
     ].join('') );
-    var endpoint = window.location.protocol + '//' + window.location.host + '/oembed?html=' + html;
+    var endpoint = window.location.protocol + '//' + window.location.host + '/oembed?type=rich&html=' + html;
     var redirectUrl = lti.launch_presentation_return_url + '?return_type=oembed&endpoint=' + encodeURIComponent(endpoint);
     window.location.href = redirectUrl;
 
