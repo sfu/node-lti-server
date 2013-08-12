d6a173fc79622769dd54c45f56d8e0e7e7fe595c
Graham Ballantyne <grahamb@sfu.ca>
2013-08-12 14:01:23 -0700

    fix endpoint url encoding

--- components/courseDescription/public/javascripts/app.js
+++ components/courseDescription/public/javascripts/app.js
@@ -4,8 +4,8 @@ var redirectToCanvas = function() {
         lti.description,
         ' &ndash; <a href="' + lti.courseUrl + '" target=_blank><em>Source: SFU Academic Calendar</em></a></p>'
     ].join('') );
-    var redirectUrl = lti.launch_presentation_return_url + '?return_type=oembed&endpoint=' + encodeURI(endpoint);
     var endpoint = window.location.protocol + '//' + window.location.host + '/oembed?html=' + html;
+    var redirectUrl = lti.launch_presentation_return_url + '?return_type=oembed&endpoint=' + encodeURIComponent(endpoint);
     window.location.href = redirectUrl;
 
 };
