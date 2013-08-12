1b1fe76e57b5ebbf77e8ed15be3700590bf01a08
Graham Ballantyne <grahamb@sfu.ca>
2013-08-12 14:46:29 -0700

    handle all oembed types

--- components/oembed/index.js
+++ components/oembed/index.js
@@ -1,16 +1,31 @@
 module.exports = function(app) {
     app.get('/oembed', function(req, res) {
-        console.log(req.query);
-        var html = req.query.html;
+        var q = req.query,
+            allowedTypes = ['photo', 'video', 'link', 'rich'];
+
+        if (allowedTypes.indexOf(q.type) < 0) {
+            res.send(500);
+            return false;
+        }
+
         var returnBody = {
-            version: "1.0",
-            type: "rich",
-            html: html,
-            width: null,
-            height: null
-        };
+            version: '1.0',
+            type: q.type,
+            width: q.width || null,
+            height: q.height || null,
+        }
+
+        if ('photo' === q.type) {
+            returnBody['url'] = q.url;
+        } else if ('link' === q.type) {
+            for (var key in q) {
+                returnBody[key] = q[key];
+            }
+        } else if ('rich' === q.type || 'video' === q.type) {
+            returnBody['html'] = q.html;
+        }
+
         res.set('Content-Type', 'application/json');
         res.send(returnBody);
     });
 };
-
