51e0d73e225b585f25919c0d9ffa8f798d489a00
Graham Ballantyne <grahamb@sfu.ca>
2014-04-29 11:21:45 -0700

    Refactor routes.js to use promises

--- components/courseOutline/routes.js
+++ components/courseOutline/routes.js
@@ -1,57 +1,57 @@
-var request = require('request');
-var utils = require('../../lib/utils.js');
-var cqUrl = 'http://www-stage.its.sfu.ca/bin/wcm/course-outlines';
-// var cqUrl = 'http://www.sfu.ca/bin/wcm/course-outlines';
-
-
-function getOutline(courseSisId, cb) {
-    request.get(cqUrl + '?' + courseSisId, function(err, resp, body){
-        cb(err, resp, body);
-    });
-}
+var utils = require('../../lib/utils');
+var outline = require('./lib/outline');
+// var cqUrl = 'http://www-stage.its.sfu.ca/bin/wcm/course-outlines';
+var cqUrl = 'http://www.sfu.ca/bin/wcm/course-outlines';
 
 exports.index = function(req, res) {
-
-    function renderError() {
-        res.render(500, {title: 'An Error Occurred', message: 'We&apos;re sorry, but an error occured while fetching your Library Reserves. Please try again later.'});
+    function renderError(template, statusCode, data) {
+        template = template || '500';
+        statusCode = statusCode || 500;
+        data = data || {
+            title: 'An Error Occurred',
+            message: 'We&apos;re sorry, but an error occured while fetching the course outline. Please try again later.'
+        }
+        res.status(statusCode)
+        res.render(template, data);
     };
 
-    function renderOutline(err, data) {
-        if (err) {
-            renderError();
-            return false;
-        }
+    function renderOutline(outlines) {
+        res.render(path.join(__dirname, 'views/index'), {outlines: outlines})
     }
 
     var courses = utils.coursesFromSisId(req.body.lis_course_offering_sourcedid || null);
+    console.log(courses);
     if (!courses) {
-        res.status(500);
-        res.render('500', {title: 'Non-Credit Course', message: 'This does not appear to be a credit course.'});
+        renderError('500', 500, {title: 'Non-Credit Course', message: 'This does not appear to be a credit course.'});
         return false;
     }
 
-    // TODO REMOVE ME
-    //
-    courses = ['1144-iat-100-d100'];
+    outline.getAllOutlines(courses).then(outline.getCanvasProfiles).then(renderOutline, renderError);
 
-    var counter = 0;
-    var outlines = [];
-    courses.forEach(function(course) {
-        getOutline(course.replace(/-/g, '/'), function(err, resp, body) {
-            counter++;
-            if (err) {
-                app.logger.error('Error fetching course outline:\n\n', err.toString());
-                renderOutline(true, null);
-                return false;
-            }
-            else {
-                outlines.push(JSON.parse(body));
-                if (counter === courses.length) {
-                    res.render(path.join(__dirname, 'views/index'), {outlines: outlines})
-                }
-            }
-        });
-    });
-
-
+    // var counter = 0;
+    // var outlines = [];
+    // courses.forEach(function(course) {
+    //     getOutline(course.replace(/-/g, '/'), function(err, resp, body) {
+    //         console.log(resp.statusCode);
+    //         counter++;
+    //         if (err || resp.statusCode !== 200) {
+    //             errorFound = true;
+    //             app.logger.error('Error fetching course outline:\n\n', err.toString());
+    //             renderOutline(true, null);
+    //             return false;
+    //         }
+    //         else {
+    //             try {
+    //                 body = JSON.parse(body);
+    //                 outlines.push(body);
+    //                 body.sisId = course;
+    //                 console.log(body);
+    //             } catch (JSONError) {
+    //                 errorFound = true;
+    //                 app.logger.error('Error parsing data from course outlines; does not appear to be valid JSON.\n\n', body, JSON.stringify(resp, null, '  '));
+    //             }
+    //             renderOutline(errorFound, outlines);
+    //         }
+    //     });
+    // });
 }
