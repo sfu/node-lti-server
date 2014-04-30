5eadfd5636eb949d4d2610ea807ba8d73f22dd6c
Graham Ballantyne <grahamb@sfu.ca>
2014-04-30 10:19:01 -0700

    Add formatDate filter

--- components/courseOutline/index.js
+++ components/courseOutline/index.js
@@ -1,9 +1,20 @@
 var routes = require('./routes.js');
 var ejs = require('ejs');
+
 module.exports = function(app) {
     ejs.filters.pluralize = function(num, str) {
         return num === 1 ? str : str + 's';
     };
+
+    ejs.filters.formatDate = function(date, format) {
+        var moment = require('moment');
+        if (!(date instanceof Date)) {
+            date = new Date(date);
+        }
+        date = moment(date);
+        return date.format(format);
+    }
+
 
     app.post('/courseOutline', routes.index);
 }
