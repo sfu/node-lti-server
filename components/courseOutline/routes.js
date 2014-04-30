d7b38953ee6ed116d5baa4a4aaef4986ebfd7e3f
Graham Ballantyne <grahamb@sfu.ca>
2014-04-30 10:18:38 -0700

    Remove old code; refactor promise

--- components/courseOutline/routes.js
+++ components/courseOutline/routes.js
@@ -26,32 +26,7 @@ exports.index = function(req, res) {
         return false;
     }
 
-    outline.getAllOutlines(courses).then(outline.getCanvasProfiles).then(renderOutline, renderError);
-
-    // var counter = 0;
-    // var outlines = [];
-    // courses.forEach(function(course) {
-    //     getOutline(course.replace(/-/g, '/'), function(err, resp, body) {
-    //         console.log(resp.statusCode);
-    //         counter++;
-    //         if (err || resp.statusCode !== 200) {
-    //             errorFound = true;
-    //             app.logger.error('Error fetching course outline:\n\n', err.toString());
-    //             renderOutline(true, null);
-    //             return false;
-    //         }
-    //         else {
-    //             try {
-    //                 body = JSON.parse(body);
-    //                 outlines.push(body);
-    //                 body.sisId = course;
-    //                 console.log(body);
-    //             } catch (JSONError) {
-    //                 errorFound = true;
-    //                 app.logger.error('Error parsing data from course outlines; does not appear to be valid JSON.\n\n', body, JSON.stringify(resp, null, '  '));
-    //             }
-    //             renderOutline(errorFound, outlines);
-    //         }
-    //     });
-    // });
+    var promise = outline.getAllOutlines(courses);
+    promise.fail(handleError);
+    promise.then(outline.getCanvasProfiles).then(renderOutline, handleError);
 }
