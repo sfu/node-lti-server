54644d5b8bd91608c90d6228950964c31c793c5b
Graham Ballantyne <grahamb@sfu.ca>
2014-05-22 14:55:08 -0700

    change routes export to a function so that it can accept the app object
    also call app.logger.error() in handleError

--- components/courseOutline/routes.js
+++ components/courseOutline/routes.js
@@ -1,41 +1,46 @@
 var utils = require('../../lib/utils');
 var outline = require('./lib/outline');
 
-exports.index = function(req, res) {
-    function handleError(reason) {
-        var template = reason.template || 'error';
-        var statusCode = reason.statusCode || 500;
-        var data = reason.data || {
-            title: 'An Error Occurred',
-            message: 'We&apos;re sorry, but an error occured while fetching the course outline. Please try again later.'
-        }
-        res.status(statusCode)
-        res.render(template, data);
-    };
 
-    function renderOutline(outlines) {
-        if (!outlines || outlines.length === 0) {
-            handleError();
+module.exports = function(app) {
+    function index(req, res) {
+        function handleError(reason) {
+            var template = reason.template || 'error';
+            var statusCode = reason.statusCode || 500;
+            var data = reason.data || {
+                title: 'An Error Occurred',
+                message: 'We&apos;re sorry, but an error occured while fetching the course outline. Please try again later.'
+            }
+            res.status(statusCode)
+            res.render(template, data);
+            app.logger.error(reason.error.message);
+        };
+
+        function renderOutline(outlines) {
+            if (!outlines || outlines.length === 0) {
+                handleError();
+                return false;
+            }
+            res.render(path.join(__dirname, 'views/index'), {outlines: outlines})
+        }
+        var courses = utils.coursesFromSisId(req.body.lis_course_offering_sourcedid || null);
+        if (!courses.length) {
+            // renderError('error', 400, {title: 'Non-Credit Course', message: 'This does not appear to be a credit course. Course outlines are only available for SFU credit courses.'});
+            handleError({
+                statusCode: 400,
+                data: {
+                    message: 'This does not appear to be a credit course. Course outlines are only available for SFU credit courses.',
+                    title: 'Non-Credit Course'
+                }
+            });
             return false;
         }
-        res.render(path.join(__dirname, 'views/index'), {outlines: outlines})
+
+        outline.getAllOutlines(courses, req.body).
+            then(outline.getCanvasProfilesForAllCourses, handleError).
+            then(renderOutline, handleError).
+            catch(handleError);
     }
 
-    var courses = utils.coursesFromSisId(req.body.lis_course_offering_sourcedid || null);
-    if (!courses.length) {
-        // renderError('error', 400, {title: 'Non-Credit Course', message: 'This does not appear to be a credit course. Course outlines are only available for SFU credit courses.'});
-        handleError({
-            statusCode: 400,
-            data: {
-                message: 'This does not appear to be a credit course. Course outlines are only available for SFU credit courses.',
-                title: 'Non-Credit Course'
-            }
-        });
-        return false;
-    }
-
-    outline.getAllOutlines(courses, req.body).
-        then(outline.getCanvasProfilesForAllCourses, handleError).
-        then(renderOutline, handleError).
-        catch(handleError);
+    return { index: index };
 }
