797b12099b2ecad9ce073825892f37162b8dc90d
Graham Ballantyne <grahamb@sfu.ca>
2014-11-05 15:24:32 -0800

    validate lti launch

--- components/rosterPhotos/index.js
+++ components/rosterPhotos/index.js
@@ -3,17 +3,38 @@ var PhotoClient = require('node-sfu-photos-client');
 var fs = require('fs');
 var path = require('path');
 var noPhotoImage = fs.readFileSync(path.resolve(__dirname, 'public/images/no_photo.png')).toString('base64');
+var LTI = require('ims-lti');
+
+function hasLaunchForCourse(req, res, next) {
+    var courseId = req.params.course;
+    if (req.session.launches && req.session.launches[courseId]) {
+        next();
+    } else {
+        res.send(403);
+    }
+}
 
 module.exports = function(app, config) {
-    var client = new PhotoClient(config.photoClient);
-    app.all('/rosterPhotos', function(req,res) {
-        if (req.method !== 'POST') {
-            res.send(405);
-            return;
-        }
-        var canvasurl = req.body.custom_canvas_api_domain;
-        var proto = req.body.launch_presentation_return_url.match(/https\:\/\//) ? 'https://' : 'http://';
-        var courseId = req.body.custom_canvas_course_id;
+    var photoClient = new PhotoClient(config.photoClient);
+    var provider = new LTI.Provider('rosterphotos', config.ltiSecret);
+
+    app.post('/rosterPhotos/launch', function(req, res) {
+        provider.valid_request(req, function(err, isValid) {
+            if (err) {
+                res.render('500', { title: 500 });
+            } else if (!isValid) {
+                // TODO redirect to a proper unauthorized page
+                res.send('403');
+            } else {
+                var courseId = provider.body.custom_canvas_course_id;
+                req.session.launches = req.session.launches || {};
+                req.session.launches[courseId] = provider;
+                req.session.launches[courseId].body = provider.body;
+                res.render(path.join(__dirname, 'views/index'), { course: provider.body, title: 'Course Roster' })
+            }
+        });
+    });
+
         var apiUrl = proto + canvasurl + '/api/v1/courses/' + courseId + '/users';
         var queryParams = {
             enrollment_type: 'student',
